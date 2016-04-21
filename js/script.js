da517d614f0c4cdb80eb3e1be24067132a4cd3cf
Amer Hamdan <amashamdan@gmail.com>
2016-04-22 00:25:24 +0300

    Refactor: Function checkTime added.
    Code repetition occured which checks the time and calls the printTime funciton.
    This repetition occured in the .click function and startBreak function.
    Repeated code was moved to the function checkTime.
    The function checks seconds and minutes and updated the time accordingly.

--- js/script.js
+++ js/script.js
@@ -6,24 +6,14 @@ $(document).ready(function() {
 		if ($(".session-input").val()){
 			var minutes = $(".session-input").val();
 		} else {
-			var minutes = 25;
+			var minutes = 0;
 		}
-		var seconds = 0;
+		var seconds = 5;
 		printTime(minutes, seconds);
 		timer = setInterval(function() {
-			if (seconds === 0 && minutes !== 0){
-				minutes--;
-				seconds = 59;
-				printTime(minutes, seconds);
-			} else if (seconds === 0 && minutes === 0) {
-				clearInterval(timer);
-				$("body").css({"backgroundColor": "#008B8B"});
-				$(".status").html("Break");
-				startBreak();
-			} else {
-				seconds--;
-				printTime(minutes, seconds);
-			}
+			var temp = checkTime(minutes, seconds, timer);
+			minutes = temp[0];
+			seconds = temp[1];
 		}, 1000);
 	})
 })
@@ -32,33 +22,32 @@ function startBreak() {
 	if ($(".break-input").val()){	
 		var minutes = $(".break-input").val();
 	} else {
-		var minutes = 5;
+		var minutes = 0;
 	}
-	var seconds = 0;
+	var seconds = 5;
 	var timer2 = setInterval(function() {
-		if (seconds === 0 && minutes !== 0){
-			minutes--;
-			seconds = 59;
-			printTime(minutes, seconds);
-		} else if (seconds === 0 && minutes === 0) {
-			clearInterval(timer2);
-			$("body").css({"backgroundColor": "#1c1c1c"});
-			$(".status").html("Session");
-		} else {
-			seconds--;
-			printTime(minutes, seconds);
-		}
+		var temp = checkTime(minutes, seconds, timer2);
+		minutes = temp[0];
+		seconds = temp[1];
 	}, 1000);
 }
 
-/*
-var aa;
-aa = checkTime();
-console.log(aa);
-
-function checkTime() {
-
-}*/
+function checkTime(minutes, seconds, timer) {
+	if (seconds === 0 && minutes !== 0){
+		minutes--;
+		seconds = 59;
+		printTime(minutes, seconds);
+	} else if (seconds === 0 && minutes === 0) {
+		clearInterval(timer);
+		$("body").css({"backgroundColor": "#008B8B"});
+		$(".status").html("Break");
+		startBreak();
+	} else {
+		seconds--;
+		printTime(minutes, seconds);
+	}
+	return [minutes, seconds];
+}
 
 function printTime(minutes, seconds) {
 	if (minutes < 10 && seconds < 10) {
